1bbf9eafd7213bb126f81b0b9b43a6a732d69c28
brynzovskii <brynzovskii@gmail.com>
2022-02-18 15:05:54 +0200

    Reusable block endpoint added to handle get request by passing block id.

--- inc/api/includes.php
+++ inc/api/includes.php
@@ -4,3 +4,8 @@
  * Default footer
  */
 require get_template_directory() . '/inc/api/endpoints/default-footer.php';
+
+/**
+ * Reusable blocks
+ */
+require get_template_directory() . '/inc/api/endpoints/reusable-blocks.php';
