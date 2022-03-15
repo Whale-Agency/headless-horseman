6fab44629e4780478ff371c0f72816528a0f1695
brynzovskii <brynzovskii@gmail.com>
2022-03-15 10:49:09 +0200

    Removed dependency plugins that are no more required

--- config/tgm/init.php
+++ config/tgm/init.php
@@ -33,7 +33,7 @@
  */
 require_once get_template_directory() . '/config/tgm/class-tgm-plugin-activation.php';
 
-add_action( 'tgmpa_register', 'headless_horseman_register_required_plugins' );
+add_action('tgmpa_register', 'headless_horseman_register_required_plugins');
 
 /**
  * Register the required plugins for this theme.
@@ -52,143 +52,138 @@ add_action( 'tgmpa_register', 'headless_horseman_register_required_plugins' );
  *
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function headless_horseman_register_required_plugins() {
-	/*
-	 * Array of plugin arrays. Required keys are name and slug.
-	 * If the source is NOT from the .org repo, then source is also required.
-	 */
-	$plugins = array(
-		// This is an example of how to include a plugin from the WordPress Plugin Repository.
-        array(
-            'name'      => 'Git Updater',
-            'slug'      => 'git-updater',
-            'source'    => get_template_directory() . '/config/tgm/plugins/git-updater-10.6.9.zip',
-            'required'  => true,
-            'external_url' => 'https://github.com/afragen/git-updater'
-        ),
+function headless_horseman_register_required_plugins()
+{
+  /*
+   * Array of plugin arrays. Required keys are name and slug.
+   * If the source is NOT from the .org repo, then source is also required.
+   */
+  $plugins = [
+    // This is an example of how to include a plugin from the WordPress Plugin Repository.
+    [
+      'name' => 'Git Updater',
+      'slug' => 'git-updater',
+      'source' => get_template_directory() . '/config/tgm/plugins/git-updater-10.6.9.zip',
+      'required' => true,
+      'external_url' => 'https://github.com/afragen/git-updater',
+    ],
 
-        array(
-            'name'      => 'REST API – Head Tags',
-            'slug'      => 'rest-api-head-tags',
-            'required'  => true,
-        ),
+    [
+      'name' => 'REST API – Head Tags',
+      'slug' => 'rest-api-head-tags',
+      'required' => true,
+    ],
 
-		array(
-			'name'      => 'Advanced Custom Fields',
-			'slug'      => 'advanced-custom-fields',
-			'required'  => false,
-            'is_callable' => 'acf'
-		),
+    [
+      'name' => 'Advanced Custom Fields',
+      'slug' => 'advanced-custom-fields',
+      'required' => false,
+      'is_callable' => 'acf',
+    ],
 
-		array(
-			'name'      => 'ACF to REST API',
-			'slug'      => 'acf-to-rest-api',
-			'required'  => false,
-		),
+    [
+      'name' => 'WP REST Cache',
+      'slug' => 'wp-rest-cache',
+      'required' => false,
+    ],
+  ];
 
-        array(
-            'name'      => 'WP REST Cache',
-            'slug'      => 'wp-rest-cache',
-            'required'  => false,
-        ),
-	);
+  /*
+   * Array of configuration settings. Amend each line as needed.
+   *
+   * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+   * strings available, please help us make TGMPA even better by giving us access to these translations or by
+   * sending in a pull-request with .po file(s) with the translations.
+   *
+   * Only uncomment the strings in the config array if you want to customize the strings.
+   */
+  $config = [
+    'id' => 'headless-horseman',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+    'default_path' => '',                      // Default absolute path to bundled plugins.
+    'menu' => 'tgmpa-install-plugins', // Menu slug.
+    'has_notices' => true,                    // Show admin notices or not.
+    'dismissable' => true,                    // If false, a user cannot dismiss the nag message.
+    'dismiss_msg' => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+    'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+    'message' => '',                      // Message to output right before the plugins table.
 
-	/*
-	 * Array of configuration settings. Amend each line as needed.
-	 *
-	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
-	 * sending in a pull-request with .po file(s) with the translations.
-	 *
-	 * Only uncomment the strings in the config array if you want to customize the strings.
-	 */
-	$config = array(
-		'id'           => 'headless-horseman',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
+    /*
+    'strings'      => array(
+      'page_title'                      => __( 'Install Required Plugins', 'headless-horseman' ),
+      'menu_title'                      => __( 'Install Plugins', 'headless-horseman' ),
+      /* translators: %s: plugin name. * /
+      'installing'                      => __( 'Installing Plugin: %s', 'headless-horseman' ),
+      /* translators: %s: plugin name. * /
+      'updating'                        => __( 'Updating Plugin: %s', 'headless-horseman' ),
+      'oops'                            => __( 'Something went wrong with the plugin API.', 'headless-horseman' ),
+      'notice_can_install_required'     => _n_noop(
+        /* translators: 1: plugin name(s). * /
+        'This theme requires the following plugin: %1$s.',
+        'This theme requires the following plugins: %1$s.',
+        'headless-horseman'
+      ),
+      'notice_can_install_recommended'  => _n_noop(
+        /* translators: 1: plugin name(s). * /
+        'This theme recommends the following plugin: %1$s.',
+        'This theme recommends the following plugins: %1$s.',
+        'headless-horseman'
+      ),
+      'notice_ask_to_update'            => _n_noop(
+        /* translators: 1: plugin name(s). * /
+        'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
+        'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
+        'headless-horseman'
+      ),
+      'notice_ask_to_update_maybe'      => _n_noop(
+        /* translators: 1: plugin name(s). * /
+        'There is an update available for: %1$s.',
+        'There are updates available for the following plugins: %1$s.',
+        'headless-horseman'
+      ),
+      'notice_can_activate_required'    => _n_noop(
+        /* translators: 1: plugin name(s). * /
+        'The following required plugin is currently inactive: %1$s.',
+        'The following required plugins are currently inactive: %1$s.',
+        'headless-horseman'
+      ),
+      'notice_can_activate_recommended' => _n_noop(
+        /* translators: 1: plugin name(s). * /
+        'The following recommended plugin is currently inactive: %1$s.',
+        'The following recommended plugins are currently inactive: %1$s.',
+        'headless-horseman'
+      ),
+      'install_link'                    => _n_noop(
+        'Begin installing plugin',
+        'Begin installing plugins',
+        'headless-horseman'
+      ),
+      'update_link' 					  => _n_noop(
+        'Begin updating plugin',
+        'Begin updating plugins',
+        'headless-horseman'
+      ),
+      'activate_link'                   => _n_noop(
+        'Begin activating plugin',
+        'Begin activating plugins',
+        'headless-horseman'
+      ),
+      'return'                          => __( 'Return to Required Plugins Installer', 'headless-horseman' ),
+      'plugin_activated'                => __( 'Plugin activated successfully.', 'headless-horseman' ),
+      'activated_successfully'          => __( 'The following plugin was activated successfully:', 'headless-horseman' ),
+      /* translators: 1: plugin name. * /
+      'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'headless-horseman' ),
+      /* translators: 1: plugin name. * /
+      'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'headless-horseman' ),
+      /* translators: 1: dashboard link. * /
+      'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'headless-horseman' ),
+      'dismiss'                         => __( 'Dismiss this notice', 'headless-horseman' ),
+      'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'headless-horseman' ),
+      'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'headless-horseman' ),
 
-		/*
-		'strings'      => array(
-			'page_title'                      => __( 'Install Required Plugins', 'headless-horseman' ),
-			'menu_title'                      => __( 'Install Plugins', 'headless-horseman' ),
-			/* translators: %s: plugin name. * /
-			'installing'                      => __( 'Installing Plugin: %s', 'headless-horseman' ),
-			/* translators: %s: plugin name. * /
-			'updating'                        => __( 'Updating Plugin: %s', 'headless-horseman' ),
-			'oops'                            => __( 'Something went wrong with the plugin API.', 'headless-horseman' ),
-			'notice_can_install_required'     => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'This theme requires the following plugin: %1$s.',
-				'This theme requires the following plugins: %1$s.',
-				'headless-horseman'
-			),
-			'notice_can_install_recommended'  => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'This theme recommends the following plugin: %1$s.',
-				'This theme recommends the following plugins: %1$s.',
-				'headless-horseman'
-			),
-			'notice_ask_to_update'            => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-				'headless-horseman'
-			),
-			'notice_ask_to_update_maybe'      => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'There is an update available for: %1$s.',
-				'There are updates available for the following plugins: %1$s.',
-				'headless-horseman'
-			),
-			'notice_can_activate_required'    => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following required plugin is currently inactive: %1$s.',
-				'The following required plugins are currently inactive: %1$s.',
-				'headless-horseman'
-			),
-			'notice_can_activate_recommended' => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following recommended plugin is currently inactive: %1$s.',
-				'The following recommended plugins are currently inactive: %1$s.',
-				'headless-horseman'
-			),
-			'install_link'                    => _n_noop(
-				'Begin installing plugin',
-				'Begin installing plugins',
-				'headless-horseman'
-			),
-			'update_link' 					  => _n_noop(
-				'Begin updating plugin',
-				'Begin updating plugins',
-				'headless-horseman'
-			),
-			'activate_link'                   => _n_noop(
-				'Begin activating plugin',
-				'Begin activating plugins',
-				'headless-horseman'
-			),
-			'return'                          => __( 'Return to Required Plugins Installer', 'headless-horseman' ),
-			'plugin_activated'                => __( 'Plugin activated successfully.', 'headless-horseman' ),
-			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'headless-horseman' ),
-			/* translators: 1: plugin name. * /
-			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'headless-horseman' ),
-			/* translators: 1: plugin name. * /
-			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'headless-horseman' ),
-			/* translators: 1: dashboard link. * /
-			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'headless-horseman' ),
-			'dismiss'                         => __( 'Dismiss this notice', 'headless-horseman' ),
-			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'headless-horseman' ),
-			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'headless-horseman' ),
+      'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
+    ),
+    */
+  ];
 
-			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
-		),
-		*/
-	);
-
-	tgmpa( $plugins, $config );
+  tgmpa($plugins, $config);
 }
